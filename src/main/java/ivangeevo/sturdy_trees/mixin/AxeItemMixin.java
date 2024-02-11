884973b2181864290f7b2f6c043256a6c6729088
Ivan Geevo <dbowgaming@gmail.com>
2024-02-11 19:27:01 +0200

    1.2 update

--- src/main/java/ivangeevo/sturdy_trees/mixin/AxeItemMixin.java
+++ src/main/java/ivangeevo/sturdy_trees/mixin/AxeItemMixin.java
@@ -1,52 +1,28 @@
 package ivangeevo.sturdy_trees.mixin;
 
-import ivangeevo.sturdy_trees.tag.SturdyTreesTags;
-import ivangeevo.sturdy_trees.util.SideModUtils;
-import net.fabricmc.loader.api.FabricLoader;
+
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.item.*;
-import net.minecraft.tag.BlockTags;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.tag.TagKey;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AxeItem.class)
-public abstract class AxeItemMixin  extends MiningToolItem implements SideModUtils {
+public class AxeItemMixin extends MiningToolItem {
     protected AxeItemMixin(float attackDamage, float attackSpeed, ToolMaterial material, TagKey<Block> effectiveBlocks, Settings settings) {
         super(attackDamage, attackSpeed, material, effectiveBlocks, settings);
     }
 
-
-
-    @Override
-    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
-
-        if (isBTWRLoaded) {
-
-            // (state.isOf(Blocks.OAK_LOG)) || (state.isOf(Blocks.SPRUCE_LOG)) || (state.isOf(Blocks.BIRCH_LOG)) || (state.isOf(Blocks.JUNGLE_LOG)) || (state.isOf(Blocks.ACACIA_LOG)) || (state.isOf(Blocks.DARK_OAK_LOG)) || (state.isOf(Blocks.MANGROVE_LOG))
-
-            if (state.isIn(BlockTags.LOGS) || state.isIn(SturdyTreesTags.Blocks.STRIPPED_LOG_BLOCKS)) {
-
-                if (stack.isOf(Items.STONE_AXE)) {
-                    return 15.0F;
-                } else if (stack.isOf(Items.IRON_AXE)) {
-                    return 35.0F;
-                } else if (stack.isOf(Items.DIAMOND_AXE)) {
-                    return 55F;
-                } else if (stack.isOf(Items.NETHERITE_AXE)) {
-                    return 70F;
-                } else if (stack.isIn(SturdyTreesTags.Items.MODDED_AXES)) {
-                    return 75F;
-                }
-
-            }
-        }
-        return super.getMiningSpeedMultiplier(stack, state);
-    }
-
-    @Override
-    public boolean isSuitableFor(BlockState state) {
-        return super.isSuitableFor(state);
+    // Removes the stripping logic
+    @Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
+    private void injectedUseOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        cir.setReturnValue(ActionResult.FAIL);
     }
 
 }
