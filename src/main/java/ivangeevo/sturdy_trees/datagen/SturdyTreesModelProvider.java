878e6a0d5f2b29094667099f977e3ee1705eee69
Ivan Geevo <dbowgaming@gmail.com>
2024-02-21 22:27:56 +0200

    testing

---
+++ src/main/java/ivangeevo/sturdy_trees/datagen/SturdyTreesModelProvider.java
@@ -0,0 +1,23 @@
+package ivangeevo.sturdy_trees.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+
+public class SturdyTreesModelProvider extends FabricModelProvider {
+    public SturdyTreesModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+    }
+
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+
+    }
+}
