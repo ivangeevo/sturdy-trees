52ddb9d3ef9e04f87f36e03b5ddbfe2c30451237
Ivan Geevo <dbowgaming@gmail.com>
2024-03-15 17:59:54 +0200

    added modern chisels to their tag in here// stump updates

--- src/main/java/ivangeevo/sturdy_trees/block/blocks/StumpBlock.java
+++ src/main/java/ivangeevo/sturdy_trees/block/blocks/StumpBlock.java
@@ -22,28 +22,6 @@ public class StumpBlock extends Block {
     @Override
     public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool) {
 
-        boolean isStumpOak = state.isOf(SturdyTreesBlocks.STUMP_OAK);
-        boolean isStumpOakVar1 = state.isOf(SturdyTreesBlocks.STUMP_OAK_VAR1);
-
-        boolean isStumpBirch = state.isOf(SturdyTreesBlocks.STUMP_BIRCH);
-        boolean isStumpBirchVar1 = state.isOf(SturdyTreesBlocks.STUMP_BIRCH_VAR1);
-
-        boolean isStumpSpruce = state.isOf(SturdyTreesBlocks.STUMP_SPRUCE);
-        boolean isStumpSpruceVar1 = state.isOf(SturdyTreesBlocks.STUMP_SPRUCE_VAR1);
-
-        boolean isStumpJungle = state.isOf(SturdyTreesBlocks.STUMP_JUNGLE);
-        boolean isStumpJungleVar1 = state.isOf(SturdyTreesBlocks.STUMP_JUNGLE_VAR1);
-
-        boolean isStumpDarkOak = state.isOf(SturdyTreesBlocks.STUMP_DARK_OAK);
-        boolean isStumpDarkOakVar1 = state.isOf(SturdyTreesBlocks.STUMP_DARK_OAK_VAR1);
-
-        boolean isStumpAcacia = state.isOf(SturdyTreesBlocks.STUMP_ACACIA);
-        boolean isStumpAcaciaVar1 = state.isOf(SturdyTreesBlocks.STUMP_ACACIA_VAR1);
-
-
-        boolean isCraftingTable = state.isOf(Blocks.CRAFTING_TABLE);
-
-
 
         if (tool.isIn(SturdyTreesTags.Items.MODERN_CHISELS)) {
 
