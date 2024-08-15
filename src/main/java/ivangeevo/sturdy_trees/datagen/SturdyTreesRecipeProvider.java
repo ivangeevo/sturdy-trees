31f94dada97a101b4aa170612ce276c30016a7e9
Ivan Geevo <dbowgaming@gmail.com>
2024-08-16 00:09:44 +0300

    1.4.1 // fix many loot tables and recipes and tags since 1.20.6 update

--- src/main/java/ivangeevo/sturdy_trees/datagen/SturdyTreesRecipeProvider.java
+++ src/main/java/ivangeevo/sturdy_trees/datagen/SturdyTreesRecipeProvider.java
@@ -1,8 +1,5 @@
 package ivangeevo.sturdy_trees.datagen;
 
-import ivangeevo.sturdy_trees.SturdyTreesItems;
-import ivangeevo.sturdy_trees.tag.SturdyTreesTags;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
@@ -11,15 +8,12 @@ import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 public class SturdyTreesRecipeProvider extends FabricRecipeProvider
 {
@@ -37,42 +31,24 @@ public class SturdyTreesRecipeProvider extends FabricRecipeProvider
 
     private void addToModRecipes(RecipeExporter exporter)
     {
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.OAK_PLANKS).input('#', Items.OAK_SLAB).pattern("#").pattern("#").criterion("has_oak_planks", RecipeProvider.conditionsFromItem(Items.OAK_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.BIRCH_PLANKS).input('#', Items.BIRCH_SLAB).pattern("#").pattern("#").criterion("has_birch_planks", RecipeProvider.conditionsFromItem(Items.BIRCH_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.SPRUCE_PLANKS).input('#', Items.SPRUCE_SLAB).pattern("#").pattern("#").criterion("has_spruce_planks", RecipeProvider.conditionsFromItem(Items.SPRUCE_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.JUNGLE_PLANKS).input('#', Items.JUNGLE_SLAB).pattern("#").pattern("#").criterion("has_jungle_planks", RecipeProvider.conditionsFromItem(Items.JUNGLE_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.ACACIA_PLANKS).input('#', Items.ACACIA_SLAB).pattern("#").pattern("#").criterion("has_acacia_planks", RecipeProvider.conditionsFromItem(Items.ACACIA_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.DARK_OAK_PLANKS).input('#', Items.DARK_OAK_SLAB).pattern("#").pattern("#").criterion("has_dark_oak_planks", RecipeProvider.conditionsFromItem(Items.DARK_OAK_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.MANGROVE_PLANKS).input('#', Items.MANGROVE_SLAB).pattern("#").pattern("#").criterion("has_mangrove_planks", RecipeProvider.conditionsFromItem(Items.MANGROVE_SLAB)).offerTo(exporter);
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.CHERRY_PLANKS).input('#', Items.CHERRY_SLAB).pattern("#").pattern("#").criterion("has_cherry_planks", RecipeProvider.conditionsFromItem(Items.CHERRY_SLAB)).offerTo(exporter);
-
+        planksFromSlab(Items.OAK_PLANKS, Items.OAK_SLAB).offerTo(exporter);
+        planksFromSlab(Items.BIRCH_PLANKS, Items.BIRCH_SLAB).offerTo(exporter);
+        planksFromSlab(Items.SPRUCE_PLANKS, Items.SPRUCE_SLAB).offerTo(exporter);
+        planksFromSlab(Items.JUNGLE_PLANKS, Items.JUNGLE_SLAB).offerTo(exporter);
+        planksFromSlab(Items.ACACIA_PLANKS, Items.ACACIA_SLAB).offerTo(exporter);
+        planksFromSlab(Items.DARK_OAK_PLANKS, Items.DARK_OAK_SLAB).offerTo(exporter);
+        planksFromSlab(Items.MANGROVE_PLANKS, Items.MANGROVE_SLAB).offerTo(exporter);
+        planksFromSlab(Items.CHERRY_PLANKS, Items.CHERRY_SLAB).offerTo(exporter);
     }
 
+    private ShapedRecipeJsonBuilder planksFromSlab(Item planks, Item slab)
+    {
+        String criterionString = "has_" + slab.toString();
+        return new ShapedRecipeJsonBuilder(RecipeCategory.BUILDING_BLOCKS, planks,1)
+                .input('#', slab)
+                .pattern("#")
+                .pattern("#")
+                .criterion(criterionString, RecipeProvider.conditionsFromItem(slab));
 
-    // Helper methods
-    public static void offerTwoInputShapelessRecipe(RecipeExporter exporter, ItemConvertible output, ItemConvertible input, ItemConvertible input2, @Nullable String group, int outputCount) {
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, output, outputCount)
-                .input(input).input(input2)
-                .group(group)
-                .criterion(hasItem(input), conditionsFromItem(input))
-                .offerTo(exporter, convertBetween(output, input));
     }
-
-    public static void offerThreeInputShapelessRecipe(RecipeExporter exporter, ItemConvertible output, ItemConvertible input, ItemConvertible input2, ItemConvertible input3, @Nullable String group, int outputCount) {
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, output, outputCount)
-                .input(input).input(input2).input(input3)
-                .group(group)
-                .criterion(hasItem(input), conditionsFromItem(input))
-                .offerTo(exporter, convertBetween(output, input));
-    }
-
-    public static void offerFourInputShapelessRecipe(RecipeExporter exporter, ItemConvertible output, ItemConvertible input, ItemConvertible input2, ItemConvertible input3, ItemConvertible input4, @Nullable String group, int outputCount) {
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, output, outputCount)
-                .input(input).input(input2).input(input3).input(input4)
-                .group(group)
-                .criterion(hasItem(input), conditionsFromItem(input))
-                .offerTo(exporter, convertBetween(output, input));
-    }
-
-
 }
