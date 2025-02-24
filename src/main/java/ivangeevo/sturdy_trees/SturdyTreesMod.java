00b7ecbbe6b762e177c50b1ae59b319ba7c08fab
Ivan Geevo <dbowgaming@gmail.com>
2025-02-24 18:21:00 +0200

    bump branch

--- src/main/java/ivangeevo/sturdy_trees/SturdyTreesMod.java
+++ src/main/java/ivangeevo/sturdy_trees/SturdyTreesMod.java
@@ -15,6 +15,5 @@ public class SturdyTreesMod implements ModInitializer {
         SturdyTreesBlocks.registerModBlocks();
         SturdyTreesItems.registerModItems();
         FuelItems.registerFuelEntries();
-
     }
 }
