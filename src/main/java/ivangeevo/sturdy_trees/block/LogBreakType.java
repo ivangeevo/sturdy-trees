79e3cbad3871830ff64d8f721b240c5ed1c6ebb1
Ivan Geevo <dbowgaming@gmail.com>
2025-05-08 18:43:41 +0300

    bump files for other branch init

--- src/main/java/ivangeevo/sturdy_trees/block/LogBreakType.java
+++ src/main/java/ivangeevo/sturdy_trees/block/LogBreakType.java
@@ -2,10 +2,14 @@ package ivangeevo.sturdy_trees.block;
 
 public enum LogBreakType {
 
-    STRIPPED,
-    CHEWED,
-    SPIKE
+    STRIPPED("stripped"),
+    CHEWED("chewed"),
+    SPIKE("spike");
 
+    private final String name;
 
+    LogBreakType(String name) {
+       this.name = name;
+    }
 
 }
