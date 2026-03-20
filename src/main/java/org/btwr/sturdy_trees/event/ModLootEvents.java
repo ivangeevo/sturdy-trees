1d4bea566558de9465d026d551f87f3af6f1c039
ivan <dbowgaming@gmail.com>
2026-03-20 17:02:31 +0200

    bump arbitrary code for loot events

--- src/main/java/org/btwr/sturdy_trees/event/ModLootEvents.java
+++ src/main/java/org/btwr/sturdy_trees/event/ModLootEvents.java
@@ -1,19 +1,18 @@
 package org.btwr.sturdy_trees.event;
 
+import com.google.common.collect.ImmutableList;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
-import net.minecraft.loot.entry.AlternativeEntry;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
-import org.btwr.shared_library.BTWRSLMod;
+import org.btwr.shared_library.mixin.accessors.ItemEntryAccessor;
+import org.btwr.shared_library.mixin.accessors.LootPoolBuilderAccessor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModLootEvents {
 
@@ -22,57 +21,35 @@ public class ModLootEvents {
             "oak", "birch", "spruce", "jungle", "acacia", "dark_oak", "mangrove", "cherry"
     };
 
+    // All vanilla saplings (excluding mangrove propagule)
+    private static final String[] vanillaSmallSaplingTypes = new String[] {
+            "oak", "birch", "spruce", "jungle", "acacia", "dark_oak", "cherry"
+    };
+
     public static void register() {
-        /**
-        LootTableEvents.REPLACE.register((key, original, source, registries) -> {
 
-            // Replace vanilla log blocks loot tables with ones from the mod
-            for (String leavesType : overworldToughWoodTypes) {
-                String path = leavesType + "_log";
-                if (key.equals(getLootTableId(path))) {
-                    return createLogLootTables(leavesType);
-                }
-            }
+    }
 
-            return original;
+    private static void modifySpecificItem(RegistryKey<LootTable> registryKey, Item target, Item toReplace) {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+
+            // Check if the key is for target's loot table
+            if (registryKey != key) return;
+
+            tableBuilder.modifyPools(builder -> {
+                List<LootPoolEntry> l = new ArrayList<>(((LootPoolBuilderAccessor) builder).getEntries().build());
+                l.replaceAll(entry -> {
+                    if (!(entry instanceof ItemEntry itemEntry))
+                        return entry;
+                    if (((ItemEntryAccessor) itemEntry).getItem().value() != target)
+                        return entry;
+                    ((ItemEntryAccessor) entry).setItem(Registries.ITEM.getEntry(toReplace));
+                    return entry;
+                });
+
+                ((LootPoolBuilderAccessor) builder).setEntries(ImmutableList.<LootPoolEntry>builder().addAll(l));
+            });
         });
-         **/
-    }
-
-    private static LootTable createLogLootTables(String type) {
-        LootPool.Builder mainPool = LootPool.builder()
-                .rolls(ConstantLootNumberProvider.create(1));
-
-        AlternativeEntry.Builder alt = AlternativeEntry.builder();
-
-        // Add the item entries based on the log type
-        for (LeafEntry.Builder<?> item : getLogLeafItemEntries(type)) {
-            alt.alternatively(item);
-        }
-        mainPool.with(alt);
-
-        return LootTable.builder().pool(mainPool).build();
-    }
-
-    private static ItemEntry.Builder<?>[] getLogLeafItemEntries(String type) {
-        // Try to find the log item dynamically based on the prefix
-        Identifier logId = Identifier.of("minecraft", type + "_log");
-        Item log = Registries.ITEM.get(logId);
-
-        // Fallback to oak log if that type doesn’t exist
-        if (log == Items.AIR) {
-            log = Items.OAK_LOG;
-        }
-
-        return new LeafEntry.Builder[]{ItemEntry.builder(log)};
-    }
-
-    private static RegistryKey<LootTable> getLootTableId(String path) {
-        return getLootTableId(BTWRSLMod.MOD_ID, path);
-    }
-
-    private static RegistryKey<LootTable> getLootTableId(String namespace, String path) {
-        return RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.of(namespace, path));
     }
 
 }
